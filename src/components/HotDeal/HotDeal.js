0a1cbb59d480f29df9f942fbbedac907990f1a94
tawsifhye <tawsif.lucse@gmail.com>
2021-10-30 10:55:58 +0600

    Hot Deals added

--- src/components/HotDeal/HotDeal.js
+++ src/components/HotDeal/HotDeal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
 import hotdeals from '../../images/hot-deals.png'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./HotDeal.css"
